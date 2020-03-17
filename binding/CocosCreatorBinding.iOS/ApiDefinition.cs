e6c04d71f7091aefe2c1209367eeeea58c813017
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2020-03-17 22:53:59 +0700

    update apidefinition XF ios

--- binding/CocosCreatorBinding.iOS/ApiDefinition.cs
+++ binding/CocosCreatorBinding.iOS/ApiDefinition.cs
@@ -11,50 +11,48 @@ namespace NativeLibrary
     {
         [Export("prefersStatusBarHidden")]
         bool PrefersStatusBarHidden { get; }
-    }
-
-    [BaseType(typeof(NSObject))]
-    interface AppController : IUIApplicationDelegate
-    {
-        [Export("viewController")]
-        RootViewController ViewController { get; }
-
-        [Export("application:didFinishLaunchingWithOptions:")]
-        bool DidFinishLaunchingWithOptions(UIApplication application, [NullAllowed] NSDictionary launchOptions);
-
-        [Export("applicationWillResignActive:")]
-        void ApplicationWillResignActive(UIApplication application);
-
-        [Export("applicationDidBecomeActive:")]
-        void ApplicationDidBecomeActive(UIApplication application);
-
-        [Export("applicationDidEnterBackground:")]
-        void ApplicationDidEnterBackground(UIApplication application);
-
-        [Export("applicationWillEnterForeground:")]
-        void ApplicationWillEnterForeground(UIApplication application);
-
-        [Export("applicationWillTerminate:")]
-        void ApplicationWillTerminate(UIApplication application);
-
-        [Export("applicationDidReceiveMemoryWarning:")]
-        void ApplicationDidReceiveMemoryWarning(UIApplication application);
 
         [Export("callNativeWithReturnBool:andContent:")]
         bool CallNativeWithReturnBool(string title, string content);
 
         [Export("callNativeWithReturnString:andContent:")]
         string CallNativeWithReturnString(string title, string content);
+    }
 
-        [Export("application:openURL:options:")]
-        bool OpenUrl(UIApplication app, NSUrl url, NSDictionary<NSString, NSObject> options);
+    [BaseType(typeof(NSObject))]
+    interface AppController
+    {
+        [Static]
+        [Export("statusBarOrientationChanged")]
+        bool StatusBarOrientationChanged();
 
-        [Export("application:openURL:sourceApplication:annotation:")]
-        bool OpenUrl(UIApplication application, NSUrl url, [NullAllowed] string sourceApplication, NSObject annotation);
+        [Static]
+        [Export("applicationWillResignActive")]
+        void ApplicationWillResignActive();
+
+        [Static]
+        [Export("applicationDidBecomeActive")]
+        void ApplicationDidBecomeActive();
+
+        [Static]
+        [Export("applicationWillTerminate")]
+        void ApplicationWillTerminate();
 
         [Static]
         [Export("evalScript:")]
         bool EvalScript(string script);
+
+        [Static]
+        [Export("initGame:")]
+        void InitGame(string key);
+
+        [Static]
+        [Export("getGameView")]
+        UIView GetGameView();
+
+        [Static]
+        [Export("startGame")]
+        void StartGame();
     }
 
     [BaseType(typeof(NSObject))]
