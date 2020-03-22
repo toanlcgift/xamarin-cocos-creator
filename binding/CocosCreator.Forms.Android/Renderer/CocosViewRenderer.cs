fc74b4f0bd0870c7dabe246b4971aea057390110
Nguyễn Đức Toàn <toanndse02449@fpt.edu.vn>
2020-03-22 09:16:43 +0700

    update

---
+++ binding/CocosCreator.Forms.Android/Renderer/CocosViewRenderer.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+using CocosCreator.Forms;
+using CocosCreator.Forms.Android;
+using Org.Cocos2dx.Lib;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CocosView), typeof(CocosViewRenderer))]
+namespace CocosCreator.Forms.Android
+{
+    public class CocosViewRenderer : ViewRenderer<CocosView, FrameLayout>
+    {
+        public static Cocos2dxActivity Cocos2DxActivity;
+        public CocosViewRenderer(Context context) : base(context)
+        {
+            
+        }
+        public CocosViewRenderer(Bundle savedInstanceState, Activity activity, Context context) : base(context)
+        {
+            Java.Lang.JavaSystem.LoadLibrary("cocos2djs");
+            Cocos2DxActivity = new Cocos2dxActivity();
+            Cocos2DxActivity.OnCreate(savedInstanceState, activity, context);
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<CocosView> e)
+        {
+            base.OnElementChanged(e);
+            SetNativeControl(Cocos2DxActivity.MFrameLayout);
+        }
+    }
+}
--- samples/CocosCreatorFormsSample.Android/MainActivity.cs
+++ samples/CocosCreatorFormsSample.Android/MainActivity.cs
@@ -9,24 +9,24 @@ using Android.OS;
 using Org.Cocos2dx.Lib;
 using Android.Content;
 using Android.Util;
+using CocosCreator.Forms.Android;
 
 namespace CocosCreatorFormsSample.Droid
 {
     [Activity(Label = "CocosCreatorFormsSample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        public static Cocos2dxActivity Cocos2DxActivity;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-            Java.Lang.JavaSystem.LoadLibrary("cocos2djs");
-            Cocos2DxActivity = new Cocos2dxActivity();
+            
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            Cocos2DxActivity.OnCreate(savedInstanceState, this, this.ApplicationContext);
+            new CocosViewRenderer(savedInstanceState, this, this.ApplicationContext);
+
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
